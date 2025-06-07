287331823f360f0ac8a8a42ac999187ee7f92b0c
h4riharan <hariharanmh001@gmail.com>
2025-06-08 00:35:56 +0530

    added main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from rembg import remove
+from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for local testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/remove-bg/")
+async def remove_bg(file: UploadFile = File(...)):
+    input_image = await file.read()
+    output_image = remove(input_image)
+    return StreamingResponse(BytesIO(output_image), media_type="image/png")
+
+@app.get("/")
+def read_root():
+    return {"message": "Background Remover API. Use /docs to try it out."}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("src.main:app", host="0.0.0.0", port=port)
